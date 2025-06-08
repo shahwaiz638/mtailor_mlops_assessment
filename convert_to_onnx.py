dacbfef3d8afcf7436fc2b9c815ea77832ced39d
Shahwaiz Memon <shahwaiz.memon@upstartcommerce.com>
2025-06-08 18:39:21 +0500

    pytorch model converted to onnx format

---
+++ convert_to_onnx.py
@@ -0,0 +1,15 @@
+from pytorch_model import Classifier, BasicBlock
+import torch
+
+mtailor = Classifier(BasicBlock, [2, 2, 2, 2])
+mtailor.load_state_dict(torch.load("./pytorch_model_weights.pth"))
+mtailor.eval()
+
+# Create example inputs for exporting the model. The inputs should be a tuple of tensors.
+example_inputs = (torch.randn(1, 3, 224, 224),)
+onnx_program = torch.onnx.export(mtailor, example_inputs, dynamo=True)
+
+
+onnx_program.optimize()
+
+onnx_program.save("onnx_model_weights.onnx")
